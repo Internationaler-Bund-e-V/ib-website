40f85d37e49a53f5628a45bd4eae61be6cd3022d
Martin Jahn <martin.jahn@ib.de>
2024-12-16 14:21:13 +0100

    Add configuration files

---
+++ public/typo3conf/AdditionalConfiguration.php
@@ -0,0 +1,12 @@
+<?php
+
+// configure solr logging
+// see https://www.schmutt.de/typo3-solr-logging/
+// mk@rms, 2023-07-11
+// $GLOBALS['TYPO3_CONF_VARS']['LOG']['ApacheSolrForTypo3']['Solr']['writerConfiguration'] = [
+//    \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
+//        'TYPO3\\CMS\\Core\\Log\\Writer\\FileWriter' => [
+//            'logFile' => \TYPO3\CMS\Core\Core\Environment::getVarPath() . '/log/solr.log'
+//        ]
+//    ]
+//];
