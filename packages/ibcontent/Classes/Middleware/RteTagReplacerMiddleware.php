5cf5b64e491e91ea476a0ab2886e739e8df08c9e
Martin Jahn <martin.jahn@ib.de>
2025-01-21 09:17:59 +0100

    Add default values for optional parameters

--- packages/ibcontent/Classes/Middleware/RteTagReplacerMiddleware.php
+++ packages/ibcontent/Classes/Middleware/RteTagReplacerMiddleware.php
@@ -83,14 +83,13 @@ class RteTagReplacerMiddleware implements MiddlewareInterface
             case 'TAG':
                 $this->standaloneView->setTemplate('GenericTag');
                 $this->template = $this->standaloneView;
-                $this->template->assignMultiple(array('genericTag' => $options));
+                $this->template->assignMultiple(values: array('genericTag' => $options));
 
                 return $this->template->render();
             case 'YT':
                 $this->standaloneView->setTemplate('YoutubeTag');
                 $this->template = $this->standaloneView;
-                // videoTitle options added
-                $this->template->assignMultiple(array('youtubecode' => $options[1], 'startAt' => $options[2], 'videoTitle' => $options[3]));
+                $this->template->assignMultiple(array('youtubecode' => $options[1], 'startAt' => $options[2] ?? 0, 'videoTitle' => $options[3] ?? ''));
 
                 return $this->template->render();
             case 'IFRAME':
