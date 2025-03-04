f866cfb650b36897e0337c2549bb13b035827920
Martin Jahn <martin.jahn@ib.de>
2025-03-04 21:21:12 +0100

    Update webpack to build backend module

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ Encore
     .addEntry('ibsearch', './packages/ibsearch/Resources/Public/JavaScript/app.ts')
     .addEntry('ib_galerie', './packages/ib_galerie/Resources/Public/JavaScript/app.ts')
     .addEntry('ib_formbuilder_frontend', './packages/ib_formbuilder/Resources/Public/JavaScript/frontend/app.js')
-    .addEntry('ib_formbuilder_backend', './packages/ib_formbuilder/Resources/Public/JavaScript/backend/app.ts')
 
     .addEntry('fwd', './packages/ibcontent/Resources/Public/JavaScript/fwd-vue/src/main.js')
     .addEntry('osmmap', './packages/ibcontent/Resources/Public/JavaScript/osmmap-vue/src/main.js')
@@ -107,4 +106,109 @@ Encore
         'ol/MapBrowserEvent': 'ol/MapBrowserEvent.js'
     });
 
-module.exports = Encore.getWebpackConfig();
+//module.exports = Encore.getWebpackConfig();
+
+// build the first configuration
+const firstConfig = Encore.getWebpackConfig();
+
+// Set a unique name for the config (needed later!)
+firstConfig.name = 'firstConfig';
+
+// reset Encore to build the second config
+Encore.reset();
+
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('packages/ib_formbuilder/Resources/Public/assets/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/assets')
+    // .setPublicPath('/typo3conf/ext/ib_template/Resources/Public/build')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('ib_formbuilder_backend', './packages/ib_formbuilder/Resources/Public/JavaScript/backend/app.ts')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    //.splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+
+    // .enableBuildNotifications()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // enables hashed filenames (e.g. app.abc123.css)
+    //.enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-transform-class-properties');
+        config, sourceType = 'unambiguous';
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    .configureImageRule({
+        // tell Webpack it should consider inlining
+        type: 'asset',
+        //maxSize: 4 * 1024, // 4 kb - the default is 8kb
+    })
+
+    .configureFontRule({
+        type: 'asset',
+        //maxSize: 4 * 1024
+    })
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+
+    // uncomment if you use TypeScript
+    .enableTypeScriptLoader()
+
+    // uncomment if you use React
+    //.enableReactPreset()
+
+    // uncomment if you use Vue.js
+    // .enableVueLoader()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    .enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    .autoProvidejQuery()
+
+
+// build the second configuration
+const secondConfig = Encore.getWebpackConfig();
+secondConfig.output = {
+    ...secondConfig.output,
+    library: {
+        name: 'formBuilder',
+        type: 'umd'
+    }
+};
+// Set a unique name for the config (needed later!)
+secondConfig.name = 'secondConfig';
+
+// export the final configuration as an array of multiple configurations
+module.exports = [firstConfig, secondConfig];
